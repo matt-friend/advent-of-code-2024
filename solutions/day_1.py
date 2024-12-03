f611fa729b6f259877aaea34db4b1fe0399a7312
matt-friend <mattfriend00@gmail.com>
2024-12-03 11:16:45 +0000

    day 1 part 2

--- solutions/day_1.py
+++ solutions/day_1.py
@@ -1,8 +1,27 @@
 import numpy as np
+from bisect import bisect_left, bisect_right
+
+
+def part_1(d):
+    d = np.sort(d)
+    return np.sum(abs(d[0]-d[1]))
+
+def count(arr, target):
+    n = len(arr)
+    left = bisect_left(arr, target, 0, n)
+    right = bisect_right(arr, target, left, n)  # use left as a lower bound
+    return right - left
+
+def similarity_score(d):
+    d = np.sort(d)
+    sim_score = 0
+    for num in d[0]:
+        sim_score += num * count(d[1], num)
+    return sim_score
+
 
 data = np.genfromtxt("data/day_1_input.txt", dtype=int)
-data = np.sort(data.T)
-
-print(np.sum(abs(data[0]-data[1])))
+data = data.T
+print(similarity_score(data))
 
 
